a9642e01cb509837aec0fb31fe0f101e35768df6
Michael Schlueter <schluet0r4life@gmail.com>
2021-12-19 16:19:10 +0100

    jsx for navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <nav
+      className="flex justify-between items-center h-16 bg-white text-black shadow-sm font-mono"
+      role="navigation"
+    >
+      <Link to="/" className="pl-8">
+        EGG
+      </Link>
+      <div className="px-4 cursor-pointer md:hidden">
+        <svg
+          className="w-8 h-8"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M4 6h16M4 12h16M4 18h16"
+          />
+        </svg>
+      </div>
+      <div className="pr-8 md:block hidden">
+          <Link to="/" className="p-4">
+              Home
+          </Link>
+          <Link to="/about" className="p-4">
+              About
+          </Link>
+          <Link to="/menu" className="p-4">
+              Menu
+          </Link>
+      </div>
+    </nav>
+  );
+};
